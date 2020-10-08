ac7547eb9df353f2838bade191dcddea310a8a16
Robinson Ramirez <robinson@camrpc.com>
2020-10-08 11:59:30 -0400

    Map 01 Solved

--- src/elm/Player.elm
+++ src/elm/Player.elm
@@ -1,10 +1,11 @@
 module Player exposing (takeTurn)
 
 import Warrior exposing (Warrior)
+import Warrior.Direction
 import Warrior.History exposing (History)
 import Warrior.Map exposing (Map)
 
 
 takeTurn : Warrior -> Map -> History -> Warrior.Action
 takeTurn warrior map history =
-    Warrior.Wait
+    Warrior.Move Warrior.Direction.Right
